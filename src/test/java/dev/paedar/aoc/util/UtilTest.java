4f549a7e3f6015880cae09de5da8cffdfb02e552
Paedar <35294944+Paedar@users.noreply.github.com>
2024-12-09 12:15:01 +0100

    Permutation test

--- src/test/java/dev/paedar/aoc/util/UtilTest.java
+++ src/test/java/dev/paedar/aoc/util/UtilTest.java
@@ -1,7 +1,33 @@
 package dev.paedar.aoc.util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UtilTest {
+
+    @Test
+    void permuteTest() {
+        var letters = List.of("A", "B");
+
+        var permutations = Util.permute(letters, 3)
+                               .map(it -> String.join("", it))
+                               .toList();
+
+        var expected = List.of("AAA",
+                               "AAB",
+                               "ABA",
+                               "ABB",
+                               "BAA",
+                               "BAB",
+                               "BBA",
+                               "BBB");
+        assertEquals(expected.size(), permutations.size());
+        assertTrue(expected.containsAll(permutations));
+    }
 
 }
