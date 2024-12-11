128c317915304590684bdbdbde379b121090e851
Paedar <35294944+Paedar@users.noreply.github.com>
2024-12-11 10:53:40 +0100

    Level 11 solution using memoization technique

--- src/main/java/dev/paedar/aoc/lvl11/AocLvl11.java
+++ src/main/java/dev/paedar/aoc/lvl11/AocLvl11.java
@@ -2,32 +2,62 @@ package dev.paedar.aoc.lvl11;
 
 import dev.paedar.aoc.util.InputReader;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class AocLvl11 {
+
+    private record LookupKey(long number, int splits) {}
+
+    private static final Map<LookupKey, Long> LOOKUP_TABLE = new HashMap<>();
 
     public static void main(String[] args) {
         var tokens = InputReader.readTokens("input_11.txt");
 
-        var numberOfStonesAfter25Blinks = numStonesAfterNUpdates(tokens, 50);
+        var numberOfStonesAfter25Blinks = numStonesAfterNUpdates(tokens, 25);
         System.out.println("I blinked 25 times and now there's this many stones: " + numberOfStonesAfter25Blinks);
+
+        var numberOfStonesAfter75Blinks = numStonesAfterNUpdates(tokens, 75);
+        System.out.println("I blinked 75 times and now there's this many stones: " + numberOfStonesAfter75Blinks);
     }
 
-    public static int numStonesAfterNUpdates(List<String> tokens, int n) {
-        var stones = new StoneList();
-        tokens.stream()
-                .map(StoneNode::of)
-                .forEach(stones::add);
+    public static long numStonesAfterNUpdates(List<String> tokens, int n) {
+        return tokens.stream()
+                     .mapToLong(Long::parseLong)
+                     .map(stoneValue -> numStonesAfterNBlinks(stoneValue, n))
+                     .sum();
+    }
 
-//        System.out.println("Before:");
-//        stones.display();
-        for(int i = 0; i < n; ++i) {
-            stones.update();
-//            System.out.println("After iteration " + (i+1));
-//            stones.display();
+    private static long numStonesAfterNBlinks(long stoneValue, int n) {
+        if (n == 0) {
+            return 1;
         }
+        var something = new LookupKey(stoneValue, n);
+        var knownValue = LOOKUP_TABLE.get(something);
+        if (knownValue == null) {
+            var calculatedValue = blinkOnce(stoneValue)
+                                          .map(it -> numStonesAfterNBlinks(it, n - 1))
+                                          .sum();
+            LOOKUP_TABLE.put(something, calculatedValue);
+            return calculatedValue;
+        }
+        return knownValue;
+    }
 
-        return stones.getSize();
+    private static LongStream blinkOnce(long stoneValue) {
+        if (stoneValue == 0) {
+            return LongStream.of(1);
+        } else if (Long.toString(stoneValue).length() % 2 == 0) {
+            var stringRep = Long.toString(stoneValue);
+            var splitAt = stringRep.length() / 2;
+            return Stream.of(stringRep.substring(0, splitAt), stringRep.substring(splitAt))
+                         .mapToLong(Long::parseLong);
+        } else {
+            return LongStream.of(stoneValue * 2024);
+        }
     }
 
 }
--- src/test/java/dev/paedar/aoc/lvl11/AocLvl11Test.java
+++ src/test/java/dev/paedar/aoc/lvl11/AocLvl11Test.java
@@ -1,7 +1,6 @@
 package dev.paedar.aoc.lvl11;
 
 import dev.paedar.aoc.util.InputReader;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,19 +12,19 @@ class AocLvl11Test {
         var tokens = InputReader.readTokens("example_11.txt");
 
         var actual = AocLvl11.numStonesAfterNUpdates(tokens, 25);
-        var expected = 55312;
+        var expected = 55312L;
 
         assertEquals(expected, actual);
     }
 
     @Test
-    @Disabled("performance reasons")
     void testStoneLineCount75BlinksExample() {
         var tokens = InputReader.readTokens("example_11.txt");
 
         var actual = AocLvl11.numStonesAfterNUpdates(tokens, 75);
-        var expected = -1; // This value is incorrect, I have not fully run the whole shebang
+        var expected = 65601038650482L;
 
         assertEquals(expected, actual);
     }
+
 }
