461e24f0b580030932080c5d595b9393a5ebc9fc
Paedar <35294944+Paedar@users.noreply.github.com>
2024-12-20 08:50:34 +0100

    Lvl 19 part 2

--- src/main/java/dev/paedar/aoc/lvl19/AocLvl19.java
+++ src/main/java/dev/paedar/aoc/lvl19/AocLvl19.java
@@ -12,11 +12,14 @@ public class AocLvl19 {
     public static void main(String[] args) {
         var lines = InputReader.readLines("input_19.txt");
 
-        var possiblePatternCount = countPossibleTowelPatterns(lines);
+        var possiblePatternCount = countPossibleTowelDesigns(lines);
         System.out.println("Possible towel patterns: " + possiblePatternCount);
+
+        var combinationCount = countAllPossibleTowelDesignsForAllPatterns(lines);
+        System.out.println("Possible combinations that lead to a valid towel pattern: " + combinationCount);
     }
 
-    public static long countPossibleTowelPatterns(List<String> lines) {
+    public static long countPossibleTowelDesigns(List<String> lines) {
         var availablePatterns = Util.splitToTokens(lines.getFirst());
         /*
         Skip over empty line in input
@@ -30,7 +33,28 @@ public class AocLvl19 {
         Map<String, Boolean> memoizedPatterns = new HashMap<>();
         availablePatterns.forEach(availablePattern -> memoizedPatterns.put(availablePattern, true));
 
-        return desiredTowelPatterns.stream().filter(towel -> isPossible(towel, availablePatterns, memoizedPatterns)).count();
+        return desiredTowelPatterns.stream()
+                                   .filter(towel -> isPossible(towel, availablePatterns, memoizedPatterns))
+                                   .count();
+    }
+
+    public static long countAllPossibleTowelDesignsForAllPatterns(List<String> lines) {
+        var availablePatterns = Util.splitToTokens(lines.getFirst());
+        /*
+        Skip over empty line in input
+         */
+        var desiredTowelPatterns = lines.subList(2, lines.size());
+
+        /*
+        Since we're writing a recursive solution, we apply memoization to reduce the amount of recursive calls
+        We cannot initialize the memoization with the available patterns in this case, because an available pattern may be composed out of other patterns
+        In the example, this is true: the patterns 'rb' and 'br' can be composed using 'r' and 'b'.
+         */
+        Map<String, Long> memoizedDesignCounts = new HashMap<>();
+
+        return desiredTowelPatterns.stream()
+                                   .mapToLong(towel -> countPossible(towel, availablePatterns, memoizedDesignCounts))
+                                   .sum();
     }
 
     private static boolean isPossible(String desiredTowelPattern, List<String> availablePatterns, Map<String, Boolean> memoizedPatterns) {
@@ -38,12 +62,29 @@ public class AocLvl19 {
             return memoizedPatterns.get(desiredTowelPattern);
         }
 
-        var desiredPatternLength = desiredTowelPattern.length();
         var isPossible = availablePatterns.stream()
                                           .filter(desiredTowelPattern::startsWith)
-                                          .anyMatch(p -> isPossible(desiredTowelPattern.substring(p.length(), desiredPatternLength), availablePatterns, memoizedPatterns));
+                                          .anyMatch(p -> isPossible(desiredTowelPattern.substring(p.length()), availablePatterns, memoizedPatterns));
         memoizedPatterns.put(desiredTowelPattern, isPossible);
         return isPossible;
+    }
+
+    private static long countPossible(String desiredTowelPattern, List<String> availablePatterns, Map<String, Long> memoizedCounts) {
+        if (memoizedCounts.containsKey(desiredTowelPattern)) {
+            return memoizedCounts.get(desiredTowelPattern);
+        }
+
+        long calculated;
+        if (desiredTowelPattern.isEmpty()) {
+            calculated = 1L;
+        } else {
+            calculated = availablePatterns.stream()
+                                              .filter(desiredTowelPattern::startsWith)
+                                              .mapToLong(p -> countPossible(desiredTowelPattern.substring(p.length()), availablePatterns, memoizedCounts))
+                                              .sum();
+        }
+        memoizedCounts.put(desiredTowelPattern, calculated);
+        return calculated;
     }
 
 }
--- src/test/java/dev/paedar/aoc/lvl19/AocLvl19Test.java
+++ src/test/java/dev/paedar/aoc/lvl19/AocLvl19Test.java
@@ -8,11 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class AocLvl19Test {
 
     @Test
-    void countPossiblePatternsExample() {
+    void countPossibleDesignsExample() {
         var lines = InputReader.readLines("example_19.txt");
 
-        var actual = AocLvl19.countPossibleTowelPatterns(lines);
+        var actual = AocLvl19.countPossibleTowelDesigns(lines);
         var expected = 6L;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void countAllPossibleTowelDesignsForAllPatternsExample() {
+        var lines = InputReader.readLines("example_19.txt");
+
+        var actual = AocLvl19.countAllPossibleTowelDesignsForAllPatterns(lines);
+        var expected = 16L;
 
         assertEquals(expected, actual);
     }
